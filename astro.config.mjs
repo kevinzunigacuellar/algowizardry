df1d39920ed77db26a7671df601e05812a05d544
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-09-29 22:08:27 -0400

    config prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,31 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Astro Wizardry',
-			logo: {
-				light: '/src/assets/logo-light.svg',
-				dark: '/src/assets/logo-dark.svg',
-				replacesTitle: true,
-			},
-			social: {
-				github: 'https://github.com/kevinzunigacuellar/algowizardry',
-			},
-			sidebar: [
-				{
-					label: 'Leetcode',
-					autogenerate: { directory: 'leetcode' },
-				},
-			],
-			customCss: [
-				'./src/styles/custom.css',
-			],
-			editLink: {
-				baseUrl: 'https://github.com/kevinzunigacuellar/algowizardry/edit/main'
-			},
-			lastUpdated: true,
-		}),
-	],
-	site: "https://algowizardry.vercel.app"
+  integrations: [
+    starlight({
+      title: "Astro Wizardry",
+      logo: {
+        light: "/src/assets/logo-light.svg",
+        dark: "/src/assets/logo-dark.svg",
+        replacesTitle: true,
+      },
+      social: {
+        github: "https://github.com/kevinzunigacuellar/algowizardry",
+      },
+      sidebar: [
+        {
+          label: "Leetcode",
+          autogenerate: { directory: "leetcode" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+      editLink: {
+        baseUrl: "https://github.com/kevinzunigacuellar/algowizardry/edit/main",
+      },
+      lastUpdated: true,
+    }),
+  ],
+  site: "https://algowizardry.vercel.app",
 });
