ab36ad3bdcfd8d3ce20229c7289bafb3c4119b5b
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-12-15 11:42:51 -0500

    Refactor MarkdownContentWrapper component and update config schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,13 +3,14 @@ import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
   docs: defineCollection({
-    schema: (ctx) =>
-      docsSchema()(ctx).extend({
+    schema: docsSchema({
+      extend: z.object({
         problemUrl: z.string().url().optional(),
         difficulty: z.enum(["easy", "medium", "hard"]).optional(),
         dataStructures: z
           .array(z.enum(["two pointers", "hash table", "stack"]))
           .optional(),
       }),
+    }),
   }),
 };
